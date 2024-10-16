55f2c6416dd9de867beb7d6802df534cc0211672
Dove-white <dlsdove@gmail.com>
2024-10-16 21:04:29 +0000

    Feat: blog card

--- app/pages/home/Blogs.tsx
+++ app/pages/home/Blogs.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import FitnessCenter from "../../../public/assets/img/Modern_Fitness-Center.jpeg";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
+import FutureOfGreenLiving from "../../../public/assets/img/Future_of_Green-Living.jpeg";
+import HomeTips from "../../../public/assets/img/Tips_for_Sustainable-Home.png";
+import UltimoGardens from "../../../public/assets/img/Ultimo-Gardens.jpeg";
 
 const Blogs = () => {
   return (
@@ -21,9 +23,18 @@ const Blogs = () => {
       </div>
 
       <div className="grid grid-cols-3 gap-[1.5rem] mt-[3rem]">
-        <CustomBlogCard />
-        <CustomBlogCard />
-        <CustomBlogCard />
+        <CustomBlogCard
+          image={FutureOfGreenLiving}
+          title="The Future of Green Living: Solar Power in Real Estate"
+        />
+        <CustomBlogCard
+          image={HomeTips}
+          title="5 Tips for a Sustainable Home"
+        />
+        <CustomBlogCard
+          image={UltimoGardens}
+          title="Ultimo Gardens: Leading Eco-Luxury in Accra"
+        />
       </div>
     </div>
   );
@@ -32,18 +43,15 @@ const Blogs = () => {
 export default Blogs;
 
 interface CustomBlogCardProps {
-  span?: number;
-  image?: string;
+  title: string;
+  image: string | StaticImageData;
 }
 export const CustomBlogCard: React.FC<CustomBlogCardProps> = ({
-  span = 1,
-  image = FitnessCenter,
+  image,
+  title,
 }) => {
   return (
-    <div
-      className="border-[0.5px] border-[#CFCFCF] p-6 rounded-[1.3rem] flex flex-col gap-[1rem]"
-      style={{ gridColumn: `span ${span} / span ${span}` }}
-    >
+    <div className="border-[0.5px] border-[#CFCFCF] p-6 rounded-[1.3rem] flex flex-col gap-[1rem]">
       <Image
         src={image}
         alt=""
@@ -53,9 +61,7 @@ export const CustomBlogCard: React.FC<CustomBlogCardProps> = ({
         <p>3rd October, 2024</p>
         <p>3 mins read</p>
       </div>
-      <h3 className="text-[#28382B] text-xl font-[700] h-[4rem]">
-        The Future of Green Living: Solar Power in Real Estate
-      </h3>
+      <h3 className="text-[#28382B] text-xl font-[700] h-[4rem]">{title}</h3>
       <button className="text-[#348841] text-[1rem] font-[700] flex gap-4 items-center mt-2">
         <span>Read more</span>
         <IoArrowForwardCircleOutline className="shrink-0" size={28} />
