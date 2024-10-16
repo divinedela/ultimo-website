14beaa422b98a3078a6a79182912dfd41638a1af
Dove-white <dlsdove@gmail.com>
2024-10-16 22:42:28 +0000

    Remove: nav list

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,6 @@ import Home from "./pages/home";
 import Footer from "./Footer";
 
 const HomePage = () => {
-  const navList = [
-    { name: "Home", link: "#" },
-    { name: "The Project", link: "#" },
-    { name: "About Us", link: "#" },
-    { name: "Gallery", link: "#" },
-    { name: "Blog", link: "#" },
-    { name: "Contact Us", link: "#" },
-  ];
 
   return (
     <div className="bg-white">
