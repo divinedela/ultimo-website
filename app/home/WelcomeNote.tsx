e969ba04ca89fd062fec3585cde5dc386c41a76f
Dove-white <dlsdove@gmail.com>
2024-10-18 18:56:50 +0000

    Fix: showForm action

--- app/home/WelcomeNote.tsx
+++ app/home/WelcomeNote.tsx
@@ -11,41 +11,56 @@ const WelcomeNote = () => {
   );
 
   return (
-    <div className="hidden md:block absolute bottom-[2rem] left-[1.5rem] z-10 text-white bg-black/30 rounded-[2rem] p-[2rem] backdrop-blur-sm w-full max-w-[33rem]">
-      <h2 className="text-[1rem] font-[700]">Welcome to Ultimo Gardens</h2>
-      <div className="flex gap-3 gap-y-0 flex-wrap">
-        {headingWords.map((word, index) => (
-          <CustomHeading
-            key={index}
-            delay={index * 0.2}
-            className={` ${index === 0 && "text-green-400 italic"}`}
-          >
-            {word} {index === 3 && <br className="hidden md:block" />}{" "}
-          </CustomHeading>
-        ))}
-      </div>
-
-      <AnimatePresence>
-        {showForm && (
-          <motion.div
-            key="tourForm"
-            initial={{ opacity: 0, y: 20, height: 0 }}
-            animate={{ opacity: 1, y: 0, height: "auto" }}
-            exit={{ opacity: 0, y: 20, height: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            <TourBookingForm />
-          </motion.div>
-        )}
-      </AnimatePresence>
-
-      {!showForm && (
-        <div className="mt-5 flex justify-between w-full gap-6">
-          <Button className="w-full" onClick={() => setShowForm(true)}>Book a tour</Button>
-          <Button className="w-full" type="secondary">Explore Property</Button>
+    <>
+      <div className="hidden md:block absolute bottom-[2rem] left-[1.5rem] z-10 text-white bg-black/30 rounded-[2rem] p-[2rem] backdrop-blur-sm w-full max-w-[33rem]">
+        <h2 className="text-[1rem] font-[700]">Welcome to Ultimo Gardens</h2>
+        <div className="flex gap-3 gap-y-0 flex-wrap">
+          {headingWords.map((word, index) => (
+            <CustomHeading
+              key={index}
+              delay={index * 0.2}
+              className={` ${index === 0 && "text-green-400 italic"}`}
+            >
+              {word} {index === 3 && <br className="hidden md:block" />}{" "}
+            </CustomHeading>
+          ))}
         </div>
+
+        <AnimatePresence>
+          {showForm && (
+            <motion.div
+              key="tourForm"
+              initial={{ opacity: 0, y: 20, height: 0 }}
+              animate={{ opacity: 1, y: 0, height: "auto" }}
+              exit={{ opacity: 0, y: 20, height: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <TourBookingForm />
+            </motion.div>
+          )}
+
+          {!showForm && (
+            <motion.div
+              key="button"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+              className="mt-5 flex justify-between w-full gap-6"
+            >
+              <Button className="w-full" onClick={() => setShowForm(true)}>
+                Book a tour
+              </Button>
+              <Button className="w-full" type="secondary">
+                Explore Property
+              </Button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      {showForm && (
+        <div onClick={() => setShowForm(false)} className="fixed inset-0" />
       )}
-    </div>
+    </>
   );
 };
 
