282daec32873dc00025a302a5a5c880f9e3ffbe2
Dove-white <dlsdove@gmail.com>
2024-10-19 23:18:55 +0000

    Feat: project page

---
+++ app/project/page.tsx
@@ -0,0 +1,27 @@
+"user client";
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+import Video from "./Video";
+import Hero from "./Hero";
+import Residences from "./Residences";
+import Experience from "./Experience";
+import Amenities from "./Amenities";
+
+const ProjectPage = () => {
+  return (
+    <div className="bg-white">
+      <Header />
+      <Hero />
+      <div className="pb-[2rem] pt-[2rem] container mx-auto  md:px-20 px-4">
+        <Residences />
+      </div>
+      <Experience />
+      <Amenities />
+      <Video />
+      <Footer />
+    </div>
+  );
+};
+
+export default ProjectPage;
