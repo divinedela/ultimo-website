d61e6a5aa659167bc70586eb50fd2c26788c5aa9
SekoViper <damehlivingstone1@gmail.com>
2024-10-20 08:27:50 +0000

    Style: button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -24,7 +24,7 @@ const Button: React.FC<ButtonProps> = ({
             ? "bg-[#348841] border-[#348841] text-white hover:bg-green-700 hover:border-green-700"
             : "border-[#D6AB11] text-[#D6AB11] hover:bg-[#D6AB11] hover:text-white"
         } 
-       px-4 py-2 rounded-xl border-2 font-semibold transition-all w-fit`,
+       px-4 py-2 rounded-xl border-2 transition-all w-fit`,
         className
       )}
     >
