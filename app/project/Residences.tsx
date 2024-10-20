1c4763a69cb3ce2c06c0dc1fe4f8578fc49b3416
Dove-white <dlsdove@gmail.com>
2024-10-20 14:51:59 +0000

    Fix: image size and text spacing

--- app/project/Residences.tsx
+++ app/project/Residences.tsx
@@ -1,30 +1,34 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import ImageLink from "../../public/assets/img/about_hero-bg.jpeg";
+import ImageLink from "../../public/assets/img/residence-ultimogardens.jpg";
+import ImageLink2 from "../../public/assets/img/residence-ultimogardens2.jpg";
 import Button from "../components/Button";
 
 const Residences = () => {
   return (
     <div className="md:pt-[4rem]">
-      <div className="grid md:grid-cols-2 gap-[2rem] md:gap-[1rem]">
-        <div className=" space-y-4 w-full max-w-[28rem]">
-          <h3 className="text-[2.25rem] tracking-[0.2em] text-wrap">
-            LUXURY DOWNTOWN{" "}
-            <span className="text-[#348841] italic">LIVING</span>
-          </h3>
-          <p className="text-[1rem] text-[#5B6B5D] leading-relaxed tracking-[0.3em]">
-            WELCOME TO HALTON PROPERTIES LOCATED IN DOWNTOWN CHARLOTTE
-          </p>
-          <div className="pt-6">
-            <Button className="rounded-none">EXPLORE RESIDENCES</Button>
+      <div className="grid md:grid-cols-2 gap-[2rem] lg:gap-0 md:gap-[1rem]">
+        <div className="flex justify-end">
+          <div className=" space-y-4 w-full sm:max-w-[33rem]">
+            <h3 className="text-[3.5rem] tracking-[0.2em] leading-none text-wrap">
+              LUXURY DOWNTOWN LIVING
+            </h3>
+            <p className="text-[1rem] text-[#5B6B5D] leading-relaxed tracking-[0.3em]">
+              WELCOME TO HALTON PROPERTIES LOCATED IN DOWNTOWN CHARLOTTE
+            </p>
+            <div className="pt-6">
+              <Button className="rounded-none font-normal text-sm py-3 px-[2rem]">
+                EXPLORE RESIDENCES
+              </Button>
+            </div>
           </div>
         </div>
-        <div className=" flex justify-end">
+        <div className=" bg-slate-400">
           <Image
             priority={false}
             src={ImageLink}
-            className="h-full max-h-[35rem] object-cover"
+            className="h-full w-full sm:min-h-[35rem] object-cover"
             alt=""
           />
         </div>
@@ -32,7 +36,7 @@ const Residences = () => {
       <div className="flex justify-end lg:justify-center -translate-y-[5rem] lg:-translate-y-[5rem] xl:-translate-y-[10rem] pr-6">
         <Image
           priority={false}
-          src={ImageLink}
+          src={ImageLink2}
           className="w-full max-w-[5rem] md:max-w-[15rem] lg:max-w-[20rem] xl:max-w-[25rem] object-cover"
           alt=""
         />
