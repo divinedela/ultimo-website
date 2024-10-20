cbc488747818b91748a4e8e1e5bb2cd20d38debc
Dove-white <dlsdove@gmail.com>
2024-10-20 14:48:17 +0000

    Fix: btn style

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -24,7 +24,7 @@ const Button: React.FC<ButtonProps> = ({
             ? "bg-[#348841] border-[#348841] text-white hover:bg-green-700 hover:border-green-700"
             : "border-[#D6AB11] text-[#D6AB11] hover:bg-[#D6AB11] hover:text-white"
         } 
-       px-4 py-2 rounded-xl border-2 transition-all w-fit`,
+       px-[2rem] py-[1rem] rounded-xl font-[700] text-[1rem] border-2 transition-all w-fit`,
         className
       )}
     >
