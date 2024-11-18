541b27612aba419fac60576954c434de98057491
Dove-white <dlsdove@gmail.com>
2024-11-18 13:47:08 +0000

    Fix: page load

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { PropsWithChildren } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: "Ultimo Garden",
   description: "Home of homes",
   icons: {
@@ -16,11 +20,35 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
+  const currentPath = usePathname();
+
+  const containerVariants = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+      transition: {
+        ease: "easeInOut",
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
     <html lang="en">
       <body className={`${inter.className} font-sans bg-white`}>
         <Header />
-        {children}
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={currentPath}
+            variants={containerVariants}
+            initial="initial"
+            whileInView="animate"
+          >
+            {children}
+          </motion.div>
+        </AnimatePresence>
         <Footer />
       </body>
     </html>
