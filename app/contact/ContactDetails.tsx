512e16d5ef4db201313f21d7062d4b16c4cfc502
Dove-white <dlsdove@gmail.com>
2024-11-18 13:20:05 +0000

    Fix: contact page

--- app/contact/ContactDetails.tsx
+++ app/contact/ContactDetails.tsx
@@ -7,22 +7,22 @@ const ContactDetails = () => {
     <div className="container mx-auto  md:px-20 px-4 pb-20">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-[1.5rem] mt-[3rem]">
         <div className="col-span-2">
-          <h3 className="text-[#28382B] text-2xl font-[700]">Find us</h3>
+          <h3 className="text-[#28382B] text-[3rem] font-[700]">Find us</h3>
         </div>
         <div className="col-span-4 space-y-6">
           <div className="flex justify-between flex-wrap sm:flex-nowrap gap-[1.5rem]">
             <div className="text-[#5B6B5D]">
-              <h3 className="text-[1rem] font-[700] mb-2">Phone:</h3>
+              <h3 className="text-[1rem] font-[700] text-[#28382B] mb-2">Phone:</h3>
               <ul className="text-sm mb-3">
                 <li>0555555555</li>
               </ul>
-              <h3 className="text-[1rem] font-[700] mb-2">Email:</h3>
+              <h3 className="text-[1rem] font-[700] text-[#28382B] mb-2">Email:</h3>
               <ul className="text-sm mb-3">
                 <li>example@ex.com</li>
               </ul>
             </div>
             <div className="text-[#5B6B5D]">
-              <h3 className="text-[1rem] font-[700] mb-3">Office Hours:</h3>
+              <h3 className="text-[1rem] font-[700] text-[#28382B] mb-3">Office Hours:</h3>
               <ul className="text-sm space-y-1">
                 <li>Monday to Friday: 9:00 AM - 6:00 PM</li>
                 <li>Saturday: 10:00 AM - 4:00 PM</li>
@@ -30,7 +30,7 @@ const ContactDetails = () => {
               </ul>
             </div>
             <div className="text-[#5B6B5D]">
-              <h3 className="text-[1rem] font-[700] mb-3">Office Address:</h3>
+              <h3 className="text-[1rem] font-[700] text-[#28382B] mb-3">Office Address:</h3>
               <ul className="text-sm space-y-1">
                 <li>Ultimo Gardens</li>
                 <li>[Insert Street Address]</li>
--- app/contact/GoogleMap.tsx
+++ app/contact/GoogleMap.tsx
@@ -10,7 +10,7 @@ const GoogleMap: React.FC = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2474.8812326201078!2d0.5265081124728485!3d5.984163473814675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1020e3c2bff56f05%3A0xc56c52f92680493d!2sJOHNNY%27S%20CORNER!5e1!3m2!1sen!2sgh!4v1723901377815!5m2!1sen!2sgh"
         width="100%"
         height="300"
-        style={{ border: 0, borderRadius: "1.3rem" }}
+        style={{ border: 0, borderRadius: "10px" }}
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
