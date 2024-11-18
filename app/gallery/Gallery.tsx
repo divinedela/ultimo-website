a975df8d048f1b5bd6d48321931b3b0b9b691775
Dove-white <dlsdove@gmail.com>
2024-11-18 12:56:26 +0000

    Fix: gallery page

--- app/gallery/Gallery.tsx
+++ app/gallery/Gallery.tsx
@@ -1,40 +1,43 @@
 "use client";
 import Image, { StaticImageData } from "next/image";
 import React from "react";
-import FutureOfGreenLiving from "../../public/assets/img/Future_of_Green-Living.jpeg";
-import HomeTips from "../../public/assets/img/Tips_for_Sustainable-Home.png";
-import UltimoGardens from "../../public/assets/img/Ultimo-Gardens.jpeg";
+import Interior from "../../public/assets/img/Interior-1.jpeg";
+import Interior2 from "../../public/assets/img/Interior-2.jpeg";
+import Interior3 from "../../public/assets/img/Interior-3.jpeg";
+import Exterior from "../../public/assets/img/Exterior-1.jpeg";
+import Exterior2 from "../../public/assets/img/Exterior-2.jpeg";
+import Exterior3 from "../../public/assets/img/Exterior-3.jpeg";
 
 const Gallery = () => {
   return (
     <div className="py-10 space-y-8">
       <div>
-        <h3 className="text-[#28382B] text-xl font-[700]">Interior</h3>
+        <h3 className="text-2xl font-[700]">Interior</h3>
 
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-[1.5rem] mt-[1rem]">
-          <CustomImageCard image={FutureOfGreenLiving} title="Dinning Area" />
-          <CustomImageCard image={HomeTips} title="Living Room 1" />
-          <CustomImageCard image={UltimoGardens} title="Living Room 3" />
+          <CustomImageCard image={Interior} title="Dinning Area" />
+          <CustomImageCard image={Interior2} title="Living Room 1" />
+          <CustomImageCard image={Interior3} title="Living Room 3" />
         </div>
       </div>
 
       <div>
-        <h3 className="text-[#28382B] text-xl font-[700]">Exterior</h3>
+        <h3 className="text-2xl font-[700]">Exterior</h3>
 
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-[1.5rem] mt-[1rem]">
-          <CustomImageCard image={FutureOfGreenLiving} title="Dinning Area" />
-          <CustomImageCard image={HomeTips} title="Living Room 1" />
-          <CustomImageCard image={UltimoGardens} title="Living Room 3" />
+          <CustomImageCard image={Exterior} title="Dinning Area" />
+          <CustomImageCard image={Exterior2} title="Living Room 1" />
+          <CustomImageCard image={Exterior3} title="Living Room 3" />
         </div>
       </div>
 
       <div>
-        <h3 className="text-[#28382B] text-xl font-[700]">Open House</h3>
+        <h3 className="text-2xl font-[700]">Open House</h3>
 
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-[1.5rem] mt-[1rem]">
-          <CustomImageCard image={FutureOfGreenLiving} title="Dinning Area" />
-          <CustomImageCard image={HomeTips} title="Living Room 1" />
-          <CustomImageCard image={UltimoGardens} title="Living Room 3" />
+          <CustomImageCard image={Interior} title="Dinning Area" />
+          <CustomImageCard image={Interior2} title="Living Room 1" />
+          <CustomImageCard image={Interior2} title="Living Room 3" />
         </div>
       </div>
     </div>
@@ -52,14 +55,14 @@ export const CustomImageCard: React.FC<CustomImageCardProps> = ({
   title,
 }) => {
   return (
-    <div className="border-[0.5px] border-[#CFCFCF] p-6 rounded-[1.3rem] flex flex-col gap-[1rem]">
+    <div className="rounded-[1.3rem] flex flex-col gap-[1rem]">
       <Image
         src={image}
         priority={false}
         alt=""
-        className="h-full max-h-[17rem] object-cover rounded-xl"
+        className="h-full max-h-[21.1rem] object-cover rounded-xl"
       />
-      <p className="text-[1rem] text-[#5B6B5D]">{title}</p>
+      <p className="text-[1rem] text-[#28382B]">{title}</p>
     </div>
   );
 };
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -5,9 +5,9 @@ const GalleryPage = () => {
   return (
     <div className="bg-white">
       <div className="pb-[2rem] pt-[2rem] container mx-auto  md:px-20 px-4">
-        <div className="space-y-2">
-          <h3 className="text-[#28382B] text-[1rem] font-[700]">Gallery</h3>
-          <h3 className="text-[2.25rem] font-[700]">
+        <div className="">
+          <h3 className="text-[#28382B] text-xl font-[700]">Gallery</h3>
+          <h3 className="text-[3rem] font-[700]">
             A glimpse into{" "}
             <span className="text-[#348841] italic">Green Luxury.</span>
           </h3>
