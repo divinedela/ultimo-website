5d47ff32428a064be25abe8d0459045dc2129ecf
SekoViper <damehlivingstone1@gmail.com>
2024-12-06 09:10:49 +0000

    Refactor: fix linter error.

--- app/components/dummy data/floor_plans.ts
+++ app/components/dummy data/floor_plans.ts
@@ -1,59 +1,63 @@
-
 export const floorPlans = [
-    {
-        id: 1,
-        name: "Floor Plan 1",
-        price: 1000,
-        image: "/assets/img/Tips_for_Sustainable-Home.png",
-        description: "This is the first floor plan of the house you are looking for. This is the first floor plan of the house you are looking for. This is the first floor plan of the house you are looking for.",
-        features: [
-            {bedroom: 3},
-            {bathroom: 2},
-            {parking: 1},
-            {sq_ft: 1050},
-            {floor: '2nd'},
-        ] 
-    },
-    {
-        id: 2,
-        name: "Floor Plan 2",
-        price: 2000,
-        image: "/assets/img/floor-plan2.png",
-        description: "This is the second floor plan of the house you are looking for. This is the second floor plan of the house you are looking for. This is the second floor plan of the house you are looking for.",
-        features: [
-            {bedroom: 2},
-            {bathroom: 1},
-            {parking: 1},
-            {sq_ft: 1000},
-            {floor: '3nd'},
-        ]
-    },
-    {
-        id: 3,
-        name: "Floor Plan 3",
-        price: 1000,
-        image: "https://static.wixstatic.com/media/eb9dc8_9c4d4186cceb49a28146bd3cf237e627~mv2.jpg/v1/fill/w_600,h_500,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/shutterstock_1018330672_edited.jpg",
-        description: "This is the first floor plan of the house you are looking for. This is the first floor plan of the house you are looking for. This is the first floor plan of the house you are looking for.",
-        features: [
-            {bedroom: 3},
-            {bathroom: 2},
-            {parking: 1},
-            {sq_ft: 1050},
-            {floor: '2nd'},
-        ] 
-    },
-    {
-        id: 4,
-        name: "Floor Plan 4",
-        price: 2000,
-        image: "/assets/img/floor-plan2.png",
-        description: "This is the second floor plan of the house you are looking for. This is the second floor plan of the house you are looking for. This is the second floor plan of the house you are looking for.",
-        features: [
-            {bedroom: 2},
-            {bathroom: 1},
-            {parking: 1},
-            {sq_ft: 1000},
-            {floor: '3nd'},
-        ]
-    },
-]
+  {
+    id: 1,
+    name: "Floor Plan 1",
+    price: 1000,
+    image: "/assets/img/Tips_for_Sustainable-Home.png",
+    description:
+      "This is the first floor plan of the house you are looking for. This is the first floor plan of the house you are looking for. This is the first floor plan of the house you are looking for.",
+    features: [
+      { bedroom: 3 },
+      { bathroom: 2 },
+      { parking: 1 },
+      { sq_ft: 1050 },
+      { floor: "2nd" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Floor Plan 2",
+    price: 2000,
+    image: "/assets/img/floor-plan2.png",
+    description:
+      "This is the second floor plan of the house you are looking for. This is the second floor plan of the house you are looking for. This is the second floor plan of the house you are looking for.",
+    features: [
+      { bedroom: 2 },
+      { bathroom: 1 },
+      { parking: 1 },
+      { sq_ft: 1000 },
+      { floor: "3nd" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Floor Plan 3",
+    price: 1000,
+    image:
+      "https://static.wixstatic.com/media/eb9dc8_9c4d4186cceb49a28146bd3cf237e627~mv2.jpg/v1/fill/w_600,h_500,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/shutterstock_1018330672_edited.jpg",
+    description:
+      "This is the first floor plan of the house you are looking for. This is the first floor plan of the house you are looking for. This is the first floor plan of the house you are looking for.",
+    features: [
+      { bedroom: 3 },
+      { bathroom: 2 },
+      { parking: 1 },
+      { sq_ft: 1050 },
+      { floor: "2nd" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Floor Plan 4",
+    price: 2000,
+    image: "/assets/img/floor-plan2.png",
+    description:
+      "This is the second floor plan of the house you are looking for. This is the second floor plan of the house you are looking for. This is the second floor plan of the house you are looking for.",
+    features: [
+      { bedroom: 2 },
+      { bathroom: 1 },
+      { parking: 1 },
+      { sq_ft: 1000 },
+      { floor: "3nd" },
+    ],
+  },
+];
